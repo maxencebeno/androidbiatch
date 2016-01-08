2c3e10177a4ccb529664452ff13659ea505829c0
Vincent Moral <vincent.moral69@yahoo.fr>
2016-01-08 11:33:39 +0100

    2 boutons fonctionnent

--- Rest/app/src/main/java/com/example/lp/rest/MainActivity.java
+++ Rest/app/src/main/java/com/example/lp/rest/MainActivity.java
@@ -1,52 +1,33 @@
 package com.example.lp.rest;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et;
+    TextView tv;
+    WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    // Listener click bouton1
+    public void onUrlButtonClick(View view){
+        webview = (WebView) findViewById(R.id.webView);
+        webview.loadUrl("http://10.0.2.2:80/androidlpmetinet/villes.php");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onHtmlButtonClick(View view){
+        webview = (WebView) findViewById(R.id.webView);
+        String strHtml = "<html><body><b> Ceci est un texte au format HTML </b></br>qui s'affiche très simplement</body></html>";
+        webview.loadData(strHtml , "text/html; charset=utf-8", "UTF-8");
     }
 }
